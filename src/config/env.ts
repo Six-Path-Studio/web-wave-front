c64a77560884faaeab31e9fc31f9f58e340afc0e
natachigram <natachigram@gmail.com>
2025-05-09 13:49:40 +0100

    env issues

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,9 +3,9 @@ const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL ||
   import.meta.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY ||
   import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const SOLANA_RPC_URL = import.meta.env.RPC_ENDPOINT ||
+const SOLANA_RPC_URL = import.meta.env.VITE_RPC_ENDPOINT ||
   "https://api.devnet.solana.com";
-const MERCHANT_PUBLIC_KEY = import.meta.env.MERCHANT_PUBLIC_KEY ||
+const MERCHANT_PUBLIC_KEY = import.meta.env.VITE_MERCHANT_PUBLIC_KEY ||
   "FScsYChomLrKYsMrpsi1RZJXtnqQKnVqywFJZAxCyc2G";
 
 // Ensure the required VITE_ env vars are provided
@@ -13,11 +13,11 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
   console.error("VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY is missing.");
 }
 
-if (!import.meta.env.RPC_ENDPOINT) {
+if (!import.meta.env.VITE_RPC_ENDPOINT) {
   console.warn("Using fallback value for Solana RPC URL");
 }
 
-if (!import.meta.env.MERCHANT_PUBLIC_KEY) {
+if (!import.meta.env.VITE_MERCHANT_PUBLIC_KEY) {
   console.warn("Using fallback value for Merchant Public Key");
 }
 
